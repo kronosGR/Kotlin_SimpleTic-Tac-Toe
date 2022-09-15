c639e6e88f57c5aa6c3f918e2a69461066e44683
KronosGR <geo.elgeo@gmail.com>
2022-09-15 16:27:32 +0200

    stage 2/5

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,8 +1,25 @@
 package tictactoe
 
+fun printBoard(array: List<String>){
+    println("---------")
+
+    for (x in 0 until array.count()) {
+        if (x == 0 || x == 3 || x == 6) {
+            print("| ")
+        }
+        print(array[x].uppercase() + " ")
+        if (x == 2 || x == 5 || x == 8) {
+            print("|")
+            println()
+        }
+    }
+
+    println("---------")
+}
 fun main() {
     // write your code here
-    println("X O X")
-    println("O X O")
-    println("X X O")
+    val input = readln()
+
+    val array: List<String> = input.toCharArray().map { it.toString() }
+    printBoard(array)
 }
